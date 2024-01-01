2200c2df49a17ecdb2ac930868ec0483c5af333f
Shubham-Zalte <shubhamzalte97@gmail.com>
2024-01-01 16:39:53 +0530

    Updated more variables

--- main.tf
+++ main.tf
@@ -76,22 +76,20 @@ resource "aws_s3_bucket" "mys3" {
 }
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-06aa3f7caf3a30282"
-  instance_type          = "t2.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.websg.id]
   subnet_id              = aws_subnet.sub1.id
   user_data              = base64encode(file("userdata.sh"))
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-06aa3f7caf3a30282"
-  instance_type          = "t2.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.websg.id]
   subnet_id              = aws_subnet.sub2.id
   user_data              = base64encode(file("userdata1.sh"))
 }
-
-#Load Balancer 
 
 resource "aws_lb" "mylb" {
   name               = "myapplb"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "ami_id" {
+  description = "ami id for Ubuntu"
+  type = string
+  default = "ami-06aa3f7caf3a30282"
+}
+
+variable "instance_type" {
+  description = "instance type for Ubuntu EC2"
+  type = string
+  default = "t2.micro"
+}
